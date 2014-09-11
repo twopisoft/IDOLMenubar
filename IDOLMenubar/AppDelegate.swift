7c2b8bb80737b893e204244c25491d97adf9a351
TwoPi <twopi@makmacair.local>
2014-09-11 10:55:06 +0800

    Initial commit

--- IDOLMenubar/AppDelegate.swift
+++ IDOLMenubar/AppDelegate.swift
@@ -12,7 +12,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet weak var window: NSWindow!
 
-
+    @IBOutlet weak var menu: NSMenu!
+    
+    var statusItem : NSStatusItem = NSStatusItem()
+    
+    override func awakeFromNib() {
+        statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+        statusItem.menu = menu
+        statusItem.highlightMode = true
+        statusItem.image = NSImage(named: "hp-logo-small")
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
     }
@@ -21,6 +31,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func preferences(sender: AnyObject) {
+        NSApp.activateIgnoringOtherApps(true)
+        self.window!.title = "Preferences"
+        self.window!.makeKeyAndOrderFront(self)
+    }
 
+    @IBAction func conceptSearch(sender: AnyObject) {
+        NSApp.activateIgnoringOtherApps(true)
+        self.window!.title = "Concept Search"
+        self.window!.makeKeyAndOrderFront(self)
+    }
+    
+    @IBAction func about(sender: AnyObject) {
+    }
+    
+    @IBAction func quit(sender: AnyObject) {
+        NSApplication.sharedApplication().terminate(self)
+    }
 }
 
