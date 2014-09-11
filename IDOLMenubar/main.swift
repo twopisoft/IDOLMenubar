1c2dfa378a691d1d1562374604aec880fc89dac5
TwoPi <twopi@makmacair.local>
2014-09-11 10:21:13 +0800

    Initial Commit

---
+++ IDOLMenubar/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  IDOLMenubar
+//
+//  Created by TwoPi on 11/9/14.
+//  Copyright (c) 2014 TwoPi. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
